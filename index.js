14d0352ce3621227bdc71db927324c04d79af2ea
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2023-07-05 20:42:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-var bs=require("./ai/brainshop.js");var dai=require("./ai/deepai.js");var p=prompt("Version");if(p.toLowerCase() === "brainshop"){console.clear();bs()}else if(p.toLowerCase() === "deepai"){console.clear();dai()}else{process.exit(0)}
+var bs=require("./ai/brainshop.js");var dai=require("./ai/deepai.js");while(true){console.clear();var p=prompt("Version");if(p.toLowerCase()==="brainshop"){console.clear();bs();break}if(p.toLowerCase()==="deepai"){console.clear();dai();break}}
