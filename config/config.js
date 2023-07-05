80e7226a248db70a8067d5505450ddf36210e8f8
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2023-07-06 06:54:05 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,1 @@
-var config=require("./config.json");module.exports ={t: process.env.token||config.token,u: process.env.url||config.url}
+var config=require("./config.json");module.exports ={t:process.env.token||config.token,u:process.env.url||config.url}
